0611d862f457e9d96db90b402ba2e75aeed0dc2f
Kwenzokuhle Dungulu <140886807+Dungul020@users.noreply.github.com>
2025-03-20 12:35:25 +0200

    serve the front-end in production

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import tasks from "./routes/tasks.js";
 import connectDB from "./utils/mongodbconnection.js";
 import cors from "cors";
 import express from "express";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 connectDB();
@@ -11,6 +12,15 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/tasks", tasks);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    });
+}
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
