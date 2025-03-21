fdd826c0d67ac18753cbc9b8189883a1954b3f4e
Kwenzokuhle Dungulu <140886807+Dungul020@users.noreply.github.com>
2025-03-21 09:23:51 +0200

    serving the front end in prod

--- server/index.js
+++ server/index.js
@@ -3,22 +3,35 @@ import tasks from "./routes/tasks.js";
 import connectDB from "./utils/mongodbconnection.js";
 import cors from "cors";
 import express from "express";
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import path from "path";
 const app = express();
 
 connectDB();
+
+const allowedOrigins = [
+    "http://localhost:3000",  
+    "https://todo-list-f11l.onrender.com",  
+  ];
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/tasks", tasks);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
-    });
-}
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "client/build")));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, "client/build", "index.html"));
+//     });
+// }
 
 
 
