f206789195f77ebb46fbc60ba0864bfe6e8f659e
Dungul020 <kwenzodungulu@gmail.com>
2025-03-21 10:49:17 +0200

    todo-app

--- server/index.js
+++ server/index.js
@@ -3,14 +3,42 @@ import tasks from "./routes/tasks.js";
 import connectDB from "./utils/mongodbconnection.js";
 import cors from "cors";
 import express from "express";
+import path from "path";
 const app = express();
 
 connectDB();
+
+const allowedOrigins = [
+    "http://localhost:3000",  
+    "https://my-chat-app-prod.onrender.com",  
+  ];
+  
+  
+  
+  
+  const __dirname = path.resolve();
+  
+ 
+  
+  app.use(
+    cors({
+      origin: allowedOrigins,
+      credentials: true, // Allow cookies
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/tasks", tasks);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
