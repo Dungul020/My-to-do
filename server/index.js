d0dd17226c32eabe4f264fd34a39b2bdc6a3fe12
Kwenzokuhle Dungulu <140886807+Dungul020@users.noreply.github.com>
2025-07-25 13:48:12 +0200

    Update index.js not to serve static files

--- server/index.js
+++ server/index.js
@@ -35,11 +35,11 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use("/api/tasks", tasks);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+//app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+//app.get("*", (req, res) => {
+//	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+//});
 
 
 app.get("/", (req, res) => {
