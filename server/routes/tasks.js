bf6249fada30bb197958d95ffe4c1936931a214d
Kwenzokuhle Dungulu <140886807+Dungul020@users.noreply.github.com>
2025-07-25 16:29:36 +0200

    Update tasks.js

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,46 +1,70 @@
+import { Component } from "react";
+import {
+    addTask,
+    getTasks,
+    updateTask,
+    deleteTask,
+} from "./services/taskServices.js";
 
-import express from "express";  
-import Task from "../models/task.js";  
+class Tasks extends Component {
+    state = { tasks: [], currentTask: "" };
 
-const router = express.Router(); 
-
-router.post("/", async (req, res) => {
-    try {
-        const task = await new Task(req.body).save();
-        res.send(task);
-    } catch (error) {
-        res.send(error);
+    async componentDidMount() {
+        try {
+            const { data } = await getTasks();
+            this.setState({ tasks: data });
+        } catch (error) {
+            console.log(error);
+        }
     }
-});
 
-router.get("/api/tasks", async (req, res) => {
-    try {
-        const tasks = await Task.find();
-         res.json(tasks); 
-    } catch (error) {
-        res.send(error);
-    }
-});
+    handleChange = ({ currentTarget: input }) => {
+        this.setState({ currentTask: input.value });
+    };
 
-router.put("/:id", async (req, res) => {
-    try {
-        const task = await Task.findOneAndUpdate(
-            { _id: req.params.id },
-            req.body
-        );
-        res.send(task);
-    } catch (error) {
-        res.send(error);
-    }
-});
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const originalTasks = this.state.tasks;
+        try {
+            const { data } = await addTask({ task: this.state.currentTask });
+            const tasks = originalTasks;
+            tasks.push(data);
+            this.setState({ tasks, currentTask: "" });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-router.delete("/:id", async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id);
-        res.send(task);
-    } catch (error) {
-        res.send(error);
-    }
-});
+    handleUpdate = async (currentTask) => {
+        const originalTasks = this.state.tasks;
+        try {
+            const tasks = [...originalTasks];
+            const index = tasks.findIndex((task) => task._id === currentTask);
+            tasks[index] = { ...tasks[index] };
+            tasks[index].completed = !tasks[index].completed;
+            this.setState({ tasks });
+            await updateTask(currentTask, {
+                completed: tasks[index].completed,
+            });
+        } catch (error) {
+            this.setState({ tasks: originalTasks });
+            console.log(error);
+        }
+    };
 
-export default router;
+    handleDelete = async (currentTask) => {
+        const originalTasks = this.state.tasks;
+        try {
+            const tasks = originalTasks.filter(
+                (task) => task._id !== currentTask
+            );
+            this.setState({ tasks });
+            await deleteTask(currentTask);
+        } catch (error) {
+            this.setState({ tasks: originalTasks });
+            console.log(error);
+        }
+    };
+}
+
+export default Tasks;
